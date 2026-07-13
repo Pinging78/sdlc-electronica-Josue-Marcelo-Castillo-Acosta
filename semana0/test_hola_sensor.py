7352bbd1a4d9761e38ef2b692582a887563fbf80
Pinging78 <josuemarcelo057@gmail.com>
2026-07-12 22:42:57 -0600

    feat: checkpoint semana 0 - sensor minimo con test

--- semana0/test_hola_sensor.py
+++ semana0/test_hola_sensor.py
@@ -1,8 +1,4 @@
 from semana0.hola_sensor import Sensor
  
 def test_sensor_reads_value():
-<<<<<<< HEAD
     assert Sensor("TEMP-01").read() == 23.5
-=======
-    assert Sensor("TEMP-01").read() == 23.5
->>>>>>> 39958771975e62daaffab82f2cb66839b451c208
