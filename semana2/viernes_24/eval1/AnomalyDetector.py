f3dc4390aed70c9eb98f98e6a66836ff4b42028b
Pinging78 <josuemarcelo057@gmail.com>
2026-07-25 20:52:28 -0600

    test: se agrego los comentarios al codigo

--- semana2/viernes_24/eval1/AnomalyDetector.py
+++ semana2/viernes_24/eval1/AnomalyDetector.py
@@ -1,8 +1,10 @@
 
 class AnomalyDetector:
-    def __init__(self, temp_max, humedad_max):
-        self.temp_max = temp_max
+    def __init__(self, temp_max, humedad_max): #valores dados en el test
+        self.temp_max = temp_max #umbral
         self.humedad_max = humedad_max
 
     def evaluar(self, temp_max, humedad_max):
         return temperatura > self.temp_max or humedad > self.humedad_max
+    #compara temperatura que se mando desde el test con el umbral y verifica si es true o false
+    #or junta las comparaciones como una compuerta or y si una es true el resultado va a ser true
