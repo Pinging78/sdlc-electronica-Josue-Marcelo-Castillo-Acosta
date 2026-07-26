6a72b8f0167e99859630cbcd868cb08c1242371f
Pinging78 <josuemarcelo057@gmail.com>
2026-07-25 21:14:58 -0600

    refactor: separa umbrales con la clase AnomalyThreshold

--- semana2/viernes_24/eval1/AnomalyDetector.py
+++ semana2/viernes_24/eval1/AnomalyDetector.py
@@ -1,10 +1,21 @@
 
-class AnomalyDetector:
+class AnomalyThreshold:
     def __init__(self, temp_max, humedad_max): #valores dados en el test
         self.temp_max = temp_max #umbral
         self.humedad_max = humedad_max
 
+class AnomalyDetector:
+    def __init__(self, threshold: AnomalyThreshold):
+        self.threshold = threshold
+
     def evaluar(self, temp_max, humedad_max):
-        return temperatura > self.temp_max or humedad > self.humedad_max
+        return self._temperatura_excedida(temperatura) or self._humedad_excedida(humedad)
     #compara temperatura que se mando desde el test con el umbral y verifica si es true o false
     #or junta las comparaciones como una compuerta or y si una es true el resultado va a ser true
+
+    def _temperatura_excedida(self, temperatura):
+        return temperatura > self.threshold.temp_max 
+#busca en el threshold el valor
+
+    def _humedad_excedida(self, humedad):
+        return humedad > self.threshold.humedad_max
--- semana2/viernes_24/eval1/test_AnomalyDetector.py
+++ semana2/viernes_24/eval1/test_AnomalyDetector.py
@@ -1,7 +1,8 @@
-from AnomalyDetector import AnomalyDetector
+from AnomalyDetector import AnomalyDetector, AnomalyThreshold
 
-#RED
+
 def test_detecta_temperatura_alta(): #se define la funcion
-    detector = AnomalyDetector(temp_max=35, humedad_max=80) #umbral maximo
+    threshold = AnomalyThreshold(temp_max=35, humedad_max=80) #umbral maximo
+    detector = AnomalyDetector(threshold) #detecta el valor del umbral y lo asigna 
     resultado = detector.evaluar(temperatura=40, humedad=50) #valores medidos ejemplo
     assert resultado is True # verifica si es cierto
