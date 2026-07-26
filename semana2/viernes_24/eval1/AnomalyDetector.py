b1d23a48655b411079238b1fe885ef2f08df7f85
Pinging78 <josuemarcelo057@gmail.com>
2026-07-25 21:31:10 -0600

    fix: corrige nombres de parametros en evaluar()

--- semana2/viernes_24/eval1/AnomalyDetector.py
+++ semana2/viernes_24/eval1/AnomalyDetector.py
@@ -8,7 +8,7 @@ class AnomalyDetector:
     def __init__(self, threshold: AnomalyThreshold):
         self.threshold = threshold
 
-    def evaluar(self, temp_max, humedad_max):
+    def evaluar(self, temperatura, humedad):
         return self._temperatura_excedida(temperatura) or self._humedad_excedida(humedad)
     #compara temperatura que se mando desde el test con el umbral y verifica si es true o false
     #or junta las comparaciones como una compuerta or y si una es true el resultado va a ser true
