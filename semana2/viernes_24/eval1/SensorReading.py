9af63b354d602d4a85406dd988fb964406fbddaa
Pinging78 <josuemarcelo057@gmail.com>
2026-07-25 23:05:47 -0600

    feat: implementar SensorReading

---
+++ semana2/viernes_24/eval1/SensorReading.py
@@ -0,0 +1,6 @@
+
+class SensorReading:
+    def __init__(self, sensor_id, temperatura, humedad):
+        self.sensor_id = sensor_id
+        self.temperatura = temperatura
+        self.humedad = humedad
