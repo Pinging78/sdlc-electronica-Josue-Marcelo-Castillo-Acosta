429076564254cf81343974d3f5b77a7c208eae03
Pinging78 <josuemarcelo057@gmail.com>
2026-07-26 00:10:06 -0600

    feat: agrega FileAlertStrategy y sus tests

--- semana2/viernes_24/eval1/AlertManager.py
+++ semana2/viernes_24/eval1/AlertManager.py
@@ -15,3 +15,13 @@ class AlertManager:
 
     def notificar(self, mensaje: str) -> None:
         self.strategy.enviar(mensaje) #como se notifica
+
+class FileAlertStrategy(AlertStrategy):
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+#se define donde queremos guardar las alertas y se guarda como atributo
+
+    def enviar(self, mensaje: str) -> None:
+        with open(self.filepath, "a") as f: #abre el archivo de forma segura y terminando el bloque lo cierra
+            f.write(mensaje + "\n") #escribe el mensaje y agrega una linea nueva para el siguiente mensaje
+#abre el archivo en la ruta guardada "a" (agregar/append) y cada nueva alerta se agrega
--- semana2/viernes_24/eval1/test_AlertManager.py
+++ semana2/viernes_24/eval1/test_AlertManager.py
@@ -1,9 +1,18 @@
-from AlertManager import AlertManager, ConsoleAlertStrategy
+from AlertManager import AlertManager, ConsoleAlertStrategy, FileAlertStrategy
 
 def test_enviar_alerta_por_consola(capsys): #capsys captura lo que imprime en la consola y lo verifica
     strategy = ConsoleAlertStrategy()
     manager = AlertManager(strategy)
-    manager.notificar("Temperatura alta en S1")
+    manager.notificar("Temperatura alta en S1") #llama a strategy.enviar() y lo imprime
     
-    capturado = capsys.readouterr()
-    assert "Temperatura alta en S1" in capturado.out
+    capturado = capsys.readouterr() #recoge lo de la consola
+    assert "Temperatura alta en S1" in capturado.out #verificamos si fue impreso
+
+def test_enviar_alerta_por_archivo(tmp_path): #carpeta temporal
+    archivo = tmp_path / "alertas.log" #ubicacion
+    strategy = FileAlertStrategy(str(archivo))
+    manager = AlertManager(strategy)
+    manager.notificar("Humedad alta en S2")
+    
+    contenido = archivo.read_text()
+    assert "Humedad alta en S2" in contenido
